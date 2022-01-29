7660aece5948f924e4efc94c7c080674128b62fd
nicolasgandrade <nicolasguerrero72@gmail.com>
2022-01-29 17:31:18 -0300

    refactor[model]: change table column generation strategy

--- src/main/java/com/nicolasgandrade/employees/model/Department.java
+++ src/main/java/com/nicolasgandrade/employees/model/Department.java
@@ -1,4 +1,27 @@
 package com.nicolasgandrade.employees.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @JsonIgnore
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees = new ArrayList<>();
 }
